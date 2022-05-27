861de6fa027c12372ab37a859ac33746bd8f74a2
zkiss97 <zkiss1997@gmail.com>
2022-05-27 11:38:43 +0200

    Add project files.

---
+++ CalculatorTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculatorTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9215eee8-2b4b-418b-b037-7f5fafb6e05d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Calculcator/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculcator
+{
+    class Khalculator
+    {
+        private double A;
+        private double B;
+        private char X;
+
+        public Khalculator()
+        {
+            this.A = A;
+            this.B = B;
+            this.X = X;
+        }
+
+        public double Addition(double A, double B)
+        {
+            return A + B;
+        }
+
+        public double Subtraction(double A, double B)
+        {
+            return A - B;
+        }
+
+        public double Multiplication(double A, double B)
+        {
+            return A * B;
+        }
+
+        public double Division(double A, double B)
+        {
+            return A / B;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            string x;
+
+            Khalculator k1 = new Khalculator();
+
+            Console.WriteLine("Infix módon add meg az adatokat:");
+            Console.WriteLine("Egyik szám:");
+            a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Műveleti jel:");
+            x = Console.ReadLine();
+
+            Console.WriteLine("Másik szám:");
+            b = double.Parse(Console.ReadLine());
+
+            if (x == "+")
+            {
+                Console.WriteLine(k1.Addition(a, b));
+            }
+            else if (x == "-")
+            {
+                Console.WriteLine(k1.Subtraction(a, b));
+            }
+            else if (x == "*")
+            {
+                Console.WriteLine(k1.Multiplication(a, b));
+            }
+            else if (x == "/")
+            {
+                Console.WriteLine(k1.Division(a, b));
+            }
+            else
+            {
+                Console.WriteLine("Érvénytelen művelet");
+            }
+
+                Console.ReadLine();
+        }
+    }
+}
